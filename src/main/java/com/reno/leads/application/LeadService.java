ab923c6ff34edfb706cdc97b1068566daa3f9079
Anand Jain <120380368+anandzambad@users.noreply.github.com>
2026-08-02 23:59:29 +0530

    feat: expose lead query through application service

--- src/main/java/com/reno/leads/application/LeadService.java
+++ src/main/java/com/reno/leads/application/LeadService.java
@@ -1,7 +1,9 @@
 package com.reno.leads.application;
 
 import com.reno.leads.api.LeadCreateRequest;
+import com.reno.leads.api.LeadResponse;
 
 public interface LeadService {
     void create(LeadCreateRequest request);
+    LeadResponse get(Long id);
 }
