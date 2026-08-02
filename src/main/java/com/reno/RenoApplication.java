e7bd5150f17d67382b37299316baca5f8145ec4e
Anand Jain <120380368+anandzambad@users.noreply.github.com>
2026-08-02 23:28:29 +0530

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/reno/RenoApplication.java
@@ -0,0 +1,11 @@
+package com.reno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RenoApplication.class, args);
+    }
+}
