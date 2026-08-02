f6ae355aaac5d7fc2c834b25093a14498fe30621
Anand Jain <120380368+anandzambad@users.noreply.github.com>
2026-08-03 00:14:33 +0530

    feat: complete lead service contract

--- src/main/java/com/reno/leads/application/LeadService.java
+++ src/main/java/com/reno/leads/application/LeadService.java
@@ -2,8 +2,13 @@ package com.reno.leads.application;
 
 import com.reno.leads.api.LeadCreateRequest;
 import com.reno.leads.api.LeadResponse;
+import com.reno.leads.api.LeadUpdateRequest;
+import java.util.List;
 
 public interface LeadService {
-    void create(LeadCreateRequest request);
+    LeadResponse create(LeadCreateRequest request);
     LeadResponse get(Long id);
+    List<LeadResponse> list();
+    LeadResponse update(Long id, LeadUpdateRequest request);
+    void delete(Long id);
 }
